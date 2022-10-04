2ca9f6d2bc867aaef1dca583a226c0c29c67aa9b
Gautama <kireichan18@gmail.com>
2022-10-04 11:17:54 +0800

    route redirect and catch all aka  not found

--- src/main.js
+++ src/main.js
@@ -5,23 +5,32 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(), // Digunakan untuk membuat suatu history didalam web browser, sehingga nantinya ngga akan ngereload lagi ketika dia mau diback or di forward.
   routes: [
     {
+      path: '/',
+      redirect: '/teams',
+    },
+    {
       path: '/teams',
       component: TeamsList,
+      alias: '/', // Alias ini digunakan untuk mengalihkan tampilan ke komponen yang bersangkutan.
     },
     {
       path: '/users',
       component: UsersList,
     },
     {
-      name: 'TeamDetail',
       path: '/teams/:teamId',
       component: TeamMembers,
       props: true,
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound,
     },
   ],
   // linkActiveClass: 'active', // Digunakan untuk aliasing active class. Kan awalnya nama dari link active classnya itu "router-link-active" tapi kalo mau ngubah bisa disini misalnya kayak "active".WW
