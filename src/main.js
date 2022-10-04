7ec7ba27b1e8610c9f9368d49ab3e07605776ddb
Gautama <kireichan18@gmail.com>
2022-10-04 08:21:27 +0800

    routing setup, registering & rendering, navigating, styling, programmatic nav

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+
+const router = createRouter({
+  history: createWebHistory(), // Digunakan untuk membuat suatu history didalam web browser, sehingga nantinya ngga akan ngereload lagi ketika dia mau diback or di forward.
+  routes: [
+    {
+      path: '/teams',
+      component: TeamsList,
+    },
+    {
+      path: '/users',
+      component: UsersList,
+    },
+  ],
+  // linkActiveClass: 'active', // Digunakan untuk aliasing active class. Kan awalnya nama dari link active classnya itu "router-link-active" tapi kalo mau ngubah bisa disini misalnya kayak "active".WW
+});
+
+const app = createApp(App);
+
+app.use(router); // method use digunakan untuk menyuruh vue menggunakan third-party library
+
+app.mount('#app');
